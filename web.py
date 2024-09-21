95ea41a52618858c93a13c188cd9d2d6b2401ed8
Yiyin1997 <904993511@qq.com>
2024-09-21 22:32:19 +0800

    web.py

--- web.py
+++ web.py
@@ -30,7 +30,7 @@ def predict(features):
     return prediction  
   
 def main():  
-    st.title("随机森林预测应用")
+    st.title("危重症患者肠内营养风险评估应用")
     st.markdown("**“使用呼吸机”、“使用镇静剂”、“使用镇痛剂”为分类变量使用=1 未使用=0**")  # 使用Markdown语法添加说明
     st.markdown("**“喂养途径” 胃管=1 肠管=0，体重单位为KG**")
   
